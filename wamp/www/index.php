d8e106a080baaeb563d48ab1ea3c073af220e533
jjpanda <pjunjie@gmail.com>
2017-03-15 20:32:21 +0800

    Add files via upload

--- wamp/www/index.php
+++ wamp/www/index.php
@@ -9,8 +9,8 @@
 <body class="w3-center">
 
 <a href="service.php">Service Information</a><br>
-<a href="stop.php">Stop Search</a><br>
-<a href="trip2.php">Trip Search</a><br>
-<a href="driver.php">Driver Search</a><br>
+<a href="trip2.php">Trip Information</a><br>
+<a href="stop.php">Stop Information</a><br>
+<a href="driver.php">Driver Information</a><br>
 </body>
 </html>
