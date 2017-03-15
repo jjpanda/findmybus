be31c0d2075b5076e4505daa99a005d023d68541
jjpanda <pjunjie@gmail.com>
2017-03-15 20:23:42 +0800

    Add files via upload

--- wamp/www/index.php
+++ wamp/www/index.php
@@ -1,8 +1,12 @@
 <html>
-<head>
-	<title>Index</title>
-</head>
-<body>
+<title>FindMyBus Index</title>
+<link rel="stylesheet" href="w3.css">
+
+<header class="w3-center"> 
+  <h1><b>FindMyBus Reporting System</b></h1>
+</header>
+
+<body class="w3-center">
 
 <a href="service.php">Service Information</a><br>
 <a href="stop.php">Stop Search</a><br>
